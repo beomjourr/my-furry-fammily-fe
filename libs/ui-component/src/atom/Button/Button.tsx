c85d2186241399b4b1c2568af9ff19749a879a81
yonghyun <lyoungh2570@gmail.com>
2023-08-14 16:40:42 +0900

    eslint 설정

--- libs/ui-component/src/atom/Button/Button.tsx
+++ libs/ui-component/src/atom/Button/Button.tsx
@@ -1,15 +1,17 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
-import styles from'./Button.module.scss';
+import styles from './Button.module.scss';
 import classNames from 'classnames';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children?: ReactNode
+  children?: ReactNode;
 }
 
 export function Button(props: ButtonProps) {
   const { children, className, ...otherProps } = props;
   return (
-    <button className={classNames(styles.base, className)} {...otherProps}>{children}</button>
+    <button className={classNames(styles.base, className)} {...otherProps}>
+      {children}
+    </button>
   );
 }
 
--- libs/ui-component/src/molecules/icon-button/icon-button.tsx
+++ libs/ui-component/src/molecules/icon-button/icon-button.tsx
@@ -1,18 +1,20 @@
-import {ReactNode} from 'react';
+import { ReactNode } from 'react';
 import Button from '../../atom/Button/Button';
 import styles from './icon-button.module.scss';
 import classnames from 'classnames';
 
 /* eslint-disable-next-line */
 export interface IconButtonProps {
-  size?: 'small' | 'medium'
-  children?: ReactNode
+  size?: 'small' | 'medium';
+  children?: ReactNode;
 }
 
 export function IconButton(props: IconButtonProps) {
-  const {children, size = 'small'} = props;
+  const { children, size = 'small' } = props;
   return (
-    <Button className={classnames(styles['icon-button'], styles[size])}>{children}</Button>
+    <Button className={classnames(styles['icon-button'], styles[size])}>
+      {children}
+    </Button>
   );
 }
 
