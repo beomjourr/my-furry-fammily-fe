514a575090777ef30c46e7aff4fa6f90cf6cbc35
yonghyun <lyoungh2570@gmail.com>
2023-08-15 19:30:59 +0900

    feat: versioning tool 추가

---
+++ .versionrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "types": [
+        {"type": "feat", "section": "Features"},
+        {"type": "fix", "section": "Bug Fixes"},
+        {"type": "revert", "section": "Revert"},
+        {"type": "chore", "hidden": true},
+    ]
+}
