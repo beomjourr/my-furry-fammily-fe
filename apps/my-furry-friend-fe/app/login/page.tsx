96dfbc83fb08f06bbf8a7f46d3a45452f388e927
WONBEEN NA <nwbnwb@naver.com>
2023-09-11 16:05:30 +0900

    feat: ongoing login page

--- apps/my-furry-friend-fe/app/login/page.tsx
+++ apps/my-furry-friend-fe/app/login/page.tsx
@@ -1,13 +1,20 @@
+import Image from 'next/image';
 import { SnsButton } from '@my-furry-family/next-ui-component';
-import { CommerceMainHeader } from '../../components/CommerceMainHeader/CommerceMainHeader';
+import logo from '@my-furry-family/images/logo.svg';
 import styles from './page.module.scss';
 
 export default async function Login() {
   return (
     <div className={styles.container}>
-      <CommerceMainHeader />
       <div className={styles.innerContainer}>
-        test 테스트
+        <div className={styles.titleWrapper}>
+          <Image src={logo} alt="logo" />
+          <div className={styles.title}>
+            <p>내 반려동물에 알맞는 제품을 추천해주는</p>
+            <p>반려동물 커머스 내새꾸 스토어</p>
+          </div>
+        </div>
+
         <SnsButton type="kakao" />
       </div>
     </div>
