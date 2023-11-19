f67aa7026efb1ba3721c8f60377a0201875772a6
beomjourr <beomjourr@gmail.com>
2023-11-19 15:45:54 +0900

    chore: commitlinit 수정 - rules에 refactor 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = { 
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'revert', 'chore']],
+        'type-enum': [2, 'always', ['feat', 'fix', 'revert', 'chore', 'refactor']],
     },
 };
