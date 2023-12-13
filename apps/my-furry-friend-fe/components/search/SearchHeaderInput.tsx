23c3f8bdfee60271b12bf3a9bec7b08336cc9b78
wonbeenna <nwbnwb@naver.com>
2023-12-13 15:26:07 +0900

    fix: input enter search

--- apps/my-furry-friend-fe/components/search/SearchHeaderInput.tsx
+++ apps/my-furry-friend-fe/components/search/SearchHeaderInput.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAtom } from 'jotai/index';
-import { debounce } from 'lodash';
 import { SearchInput } from '@my-furry-family/next-ui-component';
 import { searchKeyword } from '../../store/search';
 
 function SearchHeaderInput() {
+  const [inputValue, setInputValue] = useState('');
   const [, setKeyword] = useAtom(searchKeyword);
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  const debounceValue = debounce((value, option) => {
-    setKeyword(value);
-    setIsLoading(option.loading);
-  }, 1000);
 
   const handleChangeInput = (value: string) => {
-    setIsLoading(true);
-    debounceValue(value, { loading: false });
+    setInputValue(value);
+  };
+
+  const handleClickInput = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      setKeyword(inputValue);
+    }
   };
 
   return (
     <div className="w-full mr-[16px] ml-[10px]">
       <SearchInput
+        enterKeyHint="search"
+        onKeyPress={handleClickInput}
         onChange={(e) => {
           handleChangeInput(e.target.value);
         }}
         placeholder="병원명으로 검색해보세요."
-        isLoading={isLoading}
       />
     </div>
   );
