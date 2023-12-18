b669ff39ef80a94d20e5a114c5036554a370a85d
wonbeenna <nwbnwb@naver.com>
2023-12-18 14:39:24 +0900

    fix: api url

--- apps/my-furry-friend-fe/utils/fetcher.ts
+++ apps/my-furry-friend-fe/utils/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetcher = axios.create({
-  baseURL: 'http://3.39.36.33:8080/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 fetcher.defaults.paramsSerializer = (paramObj) => {
