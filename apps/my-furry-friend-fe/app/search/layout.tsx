d186af36903614ef9876b3ba9265b8715b2ef4d1
wonbeenna <nwbnwb@naver.com>
2024-02-07 21:32:04 +0900

    fix: keyword delete jotai

--- apps/my-furry-friend-fe/app/search/layout.tsx
+++ apps/my-furry-friend-fe/app/search/layout.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Header } from '../../components/Header/Header';
 import styles from './result/page.module.scss';
-import SearchHeaderInput from '../../components/search/SearchHeaderInput';
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles.container}>
-      <Header isBack className={styles.header}>
-        <SearchHeaderInput />
-      </Header>
-      {children}
-    </div>
-  );
+  return <div className={styles.container}>{children}</div>;
 }
 
 export default Layout;
